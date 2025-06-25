dae42e3f10c421df00ad5574849084e1d6c4b891
billelK <bilalkhansal@gmail.com>
2025-06-25 22:00:57 +0100

    boiler plate next with electron integrated

---
+++ main.js
@@ -0,0 +1,41 @@
+
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import express from 'express';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let server;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      contextIsolation: true,
+    },
+  });
+
+  const expressApp = express();
+  const outPath = path.join(__dirname, 'out');
+  expressApp.use(express.static(outPath));
+
+  server = expressApp.listen(4000, () => {
+    win.loadURL('http://localhost:4000');
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (server) server.close();
+  if (process.platform !== 'darwin') app.quit();
+});
